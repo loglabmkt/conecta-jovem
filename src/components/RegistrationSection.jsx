819e249b16cf81c38db0e00e9f2c023253b36276
Log Lab <testesappandroid@gmail.com>
2026-03-17 12:37:19 +0000

    File changes

--- src/components/RegistrationSection.jsx
+++ src/components/RegistrationSection.jsx
@@ -12,7 +12,7 @@ export default function RegistrationSection() {
     if (typeof window !== 'undefined' && window.fbq) {
       window.fbq('track', 'PageView', { content_name: 'Ver passo a passo da inscrição' }); // Track click on "Ver passo a passo"
     }
-    window.open('https://meconectei.gupy.io/jobs/9885025', '_blank');
+    window.open('https://loglabdigital.inhire.app/conecta-jovem/vagas/52d79473-4854-4af8-a6e5-b0c9f42d3e99/conecta-jovem', '_blank');
   };
 
   return (
