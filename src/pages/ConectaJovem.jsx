c16f81d6a06ee6daf25a4334018d0008ef14ce66
Log Lab <testesappandroid@gmail.com>
2026-03-18 15:10:14 +0000

    File changes

--- src/pages/ConectaJovem.jsx
+++ src/pages/ConectaJovem.jsx
@@ -13,7 +13,6 @@ import CookieConsentBanner from '../components/CookieConsentBanner';
 import AdminLogin from '../components/AdminLogin';
 import VideoPopup from '../components/VideoPopup';
 import GaleriaImagens from '../components/GaleriaImagens';
-import StackingSection from '../components/StackingSection';
 
 export default function ConectaJovem() {
   const [showAdminLogin, setShowAdminLogin] = useState(false);
