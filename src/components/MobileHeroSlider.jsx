595da82b179f962683e30b6ead399e9f42e0af16
Log Lab <testesappandroid@gmail.com>
2026-03-18 13:55:29 +0000

    File changes

--- src/components/MobileHeroSlider.jsx
+++ src/components/MobileHeroSlider.jsx
@@ -20,17 +20,14 @@ const variants = {
   enter: (direction) => ({
     x: direction > 0 ? '100%' : '-100%',
     opacity: 0,
-    scale: 0.95,
   }),
   center: {
     x: 0,
     opacity: 1,
-    scale: 1,
   },
   exit: (direction) => ({
     x: direction > 0 ? '-100%' : '100%',
     opacity: 0,
-    scale: 0.95,
   }),
 };
 
