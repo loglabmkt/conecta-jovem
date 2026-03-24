f11b460fd89c35035e12f4c768ffe3cca42290b5
Log Lab <testesappandroid@gmail.com>
2026-03-24 22:03:43 +0000

    File changes

--- src/components/RegistrationSection.jsx
+++ src/components/RegistrationSection.jsx
@@ -9,7 +9,7 @@ export default function RegistrationSection() {
   const y = useTransform(scrollY, [3000, 4000], [0, 50]);
 
   const handleLearnMore = () => {
-    window.dispatchEvent(new CustomEvent('open-inscricao', { detail: { origem: 'modal_cta' } }));
+    window.open('https://loglabdigital.inhire.app/conecta-jovem/vagas/52d79473-4854-4af8-a6e5-b0c9f42d3e99/conecta-jovem', '_blank');
   };
 
   return (
